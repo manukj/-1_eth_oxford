15e790218efc3263d5d16e885dcc2d7d45ec800f
Manu <manu1998kj@gmail.com>
2024-03-09 23:39:06 +0000

    added dual investment condition

--- contract/contracts/DualInvestment.sol
+++ contract/contracts/DualInvestment.sol
@@ -1,62 +1,90 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-// contract DualInvestment {
-//     address public usdcTokenAddress =
-//         0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // Mainnet USDC Token Address in BSC
-//     address public ethTokenAddress = 0x2170Ed0880ac9A755fd29B2688956BD959F933F8; // Mainnet ETH Token Address in BSC
-//     AggregatorV3Interface internal dataFeed;
-//     receive() external payable {}
-
-//     // write a function to recieve amount of USDC which will be used to buy ETH
-//     function buyEth(uint256 _amount) public {
-//         // the amount of USDC will be transferred from the sender to the contract
-//         IERC20(usdcTokenAddress).transferFrom(
-//             msg.sender,
-//             address(this),
-//             _amount
-//         );
-//         int ethPrice = getLatestPrice();
-//         uint256 ethAmount = _amount / ethPrice;
-//         // require(address(this).balance >= ethAmount, "Insufficient balance in the contract");
-//         // payable(msg.sender).transfer(ethAmount);
-//     }
-
-//     function placeOrder(uint256 _amount) public {
-//         int ethPrice = getLatestPrice();
-//         uint256 ethAmount = _amount / ethPrice;
-//         // emit a event with the amount of ETH can be bought
-//         // require(address(this).balance >= ethAmount, "Insufficient balance in the contract");
-//         // payable(msg.sender).transfer(ethAmount);
-//     }
-
-//     constructor() {
-//         dataFeed = AggregatorV3Interface(
-//             0x9ef1B8c0E4F7dc8bF5719Ea496883DC6401d5b2e
-//         );
-//     }
-
-//     function getLatestPrice() public view returns (int) {
-//         (, int currentEthPrice, , , ) = dataFeed.latestRoundData();
-//         return answer;
-//     }
-// }
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DualInvestment {
     address public usdcTokenAddress;
     address public ethTokenAddress;
+    address public priceETHUSDAddress =
+        0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    AggregatorV3Interface internal priceFeed;
+
+    struct Order {
+        address user;
+        uint256 ethAmount;
+        uint256 ethPriceAtOrder;
+        uint256 margin; // Percentage gain desired
+        uint256 timestamp;
+        bool executed;
+    }
+
+    mapping(address => Order) public orders;
 
     constructor(address _usdcTokenAddress, address _ethTokenAddress) {
         usdcTokenAddress = _usdcTokenAddress;
         ethTokenAddress = _ethTokenAddress;
+        priceFeed = AggregatorV3Interface(priceETHUSDAddress);
     }
 
-    // recieve USDC token from a address and stored in the contract address
+    function getLatestPrice() public view returns (int) {
+        (, int price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function placeOrderToSellEthLow(uint256 _amount, uint256 _margin) public {
+        int ethPrice = getLatestPrice();
+        uint256 ethAmount = _amount / uint256(ethPrice);
+        IERC20(ethTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            ethAmount
+        );
+
+        orders[msg.sender] = Order(
+            msg.sender,
+            ethAmount,
+            uint256(ethPrice),
+            _margin,
+            block.timestamp,
+            false
+        );
+    }
+
+    function executeOrder(address _user) public {
+        Order storage order = orders[_user];
+        require(order.user != address(0), "Order does not exist");
+        require(!order.executed, "Order already executed");
+
+        // Check if the current ETH price has reached the desired margin
+        uint256 currentEthInUSDPrice = uint256(getLatestPrice());
+        uint256 currentUsdcAmount = currentEthInUSDPrice * order.ethAmount;
+        uint256 originalUsdcAmount = order.ethPriceAtOrder * order.ethAmount;
+        uint256 gain = (currentUsdcAmount * 100) / originalUsdcAmount;
+
+        if (
+            gain >= order.margin || block.timestamp >= order.timestamp + 6 days
+        ) {
+            // Margin reached or 6 days passed, execute order
+            uint256 marginUsdcAmount = (originalUsdcAmount * order.margin) / 100;
+            uint256 remainingUsdcAmount = currentUsdcAmount - marginUsdcAmount;
+            IERC20(usdcTokenAddress).transfer(order.user, marginUsdcAmount);
+            order.executed = true;
+            if (remainingUsdcAmount > 0) {
+                IERC20(usdcTokenAddress).transfer(address(this), remainingUsdcAmount);
+            }
+        } else {
+            // Return a fraction of ETH to the user
+            uint256 refundEthAmount = order.ethAmount + order.ethAmount / 100; // 1% of the initial ETH amount
+            IERC20(ethTokenAddress).transfer(order.user, refundEthAmount);
+            order.executed = true;
+        }
+    }
+
     function recieveUSDC(uint256 _amount) public {
-        // the amount of USDC will be transferred from the sender to the contract
         IERC20(usdcTokenAddress).transferFrom(
             msg.sender,
             address(this),
@@ -64,23 +92,11 @@ contract DualInvestment {
         );
     }
 
-    // recieve ETH token from a address and stored in the contract address
     function recieveETH(uint256 _amount) public {
-        // the amount of USDC will be transferred from the sender to the contract
         IERC20(ethTokenAddress).transferFrom(
             msg.sender,
             address(this),
             _amount
         );
-    }
-
-    // function to buy ETH using USDC with a date   
-    function buyEth(uint256 _amount) public {
-        // get the current price of ETH
-        int ethPrice = getLatestPrice();
-        // calculate the amount of ETH that can be bought
-        uint256 ethAmount = _amount / ethPrice;
-        // transfer the amount of ETH to the sender
-        IERC20(ethTokenAddress).transfer(msg.sender, ethAmount);
     }
 }
